472cde3b41200b627d9fb69aa849f75d41dff6f6
Brett Polacek <brettpolacek@gmail.com>
2023-01-13 14:39:19 +0100

    commit tree 99184c5f6a634c04e02a2e0cd245cab9fc0b29b0

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+/*function add5() {
+    return (number += 5);
+  }
+
+//function number(--);
+
+/*function makeInt() {
+    return (1, 3, 10);
+}*/
